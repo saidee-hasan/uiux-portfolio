2889618dc00d52f4d6935a1e539e491e6e0b6cda
wptasmina <wptasmina@gmail.com>
2025-05-27 13:36:05 +0600

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 
 import Navbar from "@/components/navbar";
 import AnimatedCursor from "react-animated-cursor"
+
 export default function Home() {
   return (
  <div className='min-h-screen bg-gray-900'>
   <Navbar />
   <h1 className="text-4xl text-white">hellow</h1>
+
+<div className="bg-gradient-primary animate-float-up p-8 rounded-md">
+  <h1 className="text-3xl font-bold">Hello with Float Up</h1>
+</div>
+
+{/* <div className="animate-bounce text-white">Bouncing Element</div> */}
+
+{/* <div className="animate-levitate text-white bg-gradient-primary ">I float softly</div> */}
 
 
   <AnimatedCursor
