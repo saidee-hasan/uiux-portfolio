e309de5f96226b5d6b9764ae7c7a8b50d40a7737
wptasmina <wptasmina@gmail.com>
2025-06-02 22:12:12 +0600

    Icon add

--- components/ContactUs/SidebarCard.tsx
+++ components/ContactUs/SidebarCard.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/card";
 import { MagicCard } from "../magicui/magic-card";
 import { useTheme } from "next-themes";
-import { IconMapPin } from "@tabler/icons-react";
+import { IconHomeSpark, IconMapPin } from "@tabler/icons-react";
 
 export default function SidebarCard() {
   const { theme } = useTheme();
@@ -40,7 +40,7 @@ export default function SidebarCard() {
                 {/* Two columns below on medium screens */}
                 <div className="border p-4 rounded-md md:col-span-2 shadow text-center">
                   <div className="w-16 h-16 mx-auto flex items-center justify-center border-2 border-dashed rounded-full">
-                    <IconMapPin className="w-6 h-6 " />
+                    <IconHomeSpark className="w-6 h-6 " />
                 </div>
                  social Icin
                 </div>
