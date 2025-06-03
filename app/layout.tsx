6bda0f94d77582c58670b8181f3d4dbfdbe46e33
wptasmina <wptasmina@gmail.com>
2025-06-03 14:17:42 +0600

    delet footer link

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import "./globals.css";
 import CustomCursor from "@/components/shared/CustomCursor";
 import { ThemeProvider } from "@/components/shared/ThemeProvider";
 import Header from "@/components/Header/Header";
-import Footer from "@/components/shared/Footer";
-
 
 
 
@@ -44,7 +42,7 @@ export default function RootLayout({
           <Header />
           <CustomCursor />
           <main className="min-h-[calc(100vh-69px)] ">{children}</main>
-          <Footer />
+
         </ThemeProvider>
       </body>
     </html>
