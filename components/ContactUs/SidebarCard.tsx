1ca100f7a2b975892262cf048a6459c05813f443
wptasmina <wptasmina@gmail.com>
2025-06-03 13:20:31 +0600

    Fix: SidebarCard component path for Vercel build

--- components/ContactUs/SidebarCard.tsx
+++ components/ContactUs/SidebarCard.tsx
@@ -42,7 +42,7 @@ export default function SidebarCard() {
               <div className="grid grid-cols-1 md:grid-cols-4 gap-6 px-4 py-8">
                 {/* Full-width section on all screen sizes */}
                 <div className="border md:col-span-4 p-4 rounded-md font-medium shadow text-center">
-                  <div className="w-16 h-16 mx-auto flex items-center justify-center border-2 border-dashed rounded-full">
+                  <div className="w-16 h-16 mx-auto flex items-center justify-center border-2 border-dashed shadow-xl rounded-full">
                     <IconBuildingCommunity className="w-6 h-6 " />
                   </div>
                   <div className="mt-4">Sylhet, Bangladesh</div>
@@ -50,11 +50,10 @@ export default function SidebarCard() {
 
                 {/* Two columns below on medium screens and icons */}
                 <div className="border rounded-md md:col-span-2 shadow text-center">
-                  <div className="w-16 h-16 mt-4 mx-auto flex items-center justify-center border-2 border-dashed rounded-full">
+                  <div className="w-16 h-16 mt-4 mx-auto flex items-center justify-center border-2 border-dashed shadow-xl rounded-full">
                     <IconDevicesSearch className="w-6 h-6 " />
                   </div>
 
-                  {/* <div className="mt-4 border p-4"> */}
                   <div className="mt-4 border-t py-2 bg-black flex flex-row items-center justify-center space-x-2">
                     {[
                       {
@@ -87,11 +86,11 @@ export default function SidebarCard() {
                       </a>
                     ))}
                   </div>
-                  {/* </div> */}
+
                 </div>
                 {/* Two columns below on medium screens  */}
                 <div className="border p-4 rounded-md md:col-span-2 shadow">
-                  <div className="w-16 h-16 mx-auto flex items-center justify-center border-2 border-dashed rounded-full">
+                  <div className="w-16 h-16 mx-auto flex items-center justify-center border-2 border-dashed shadow-xl rounded-full">
                     <IconMailSpark className="w-6 h-6 " />
                   </div>
                   <div className="mt-4 text-xs text-gray-400">
